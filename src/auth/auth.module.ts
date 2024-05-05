800810e5e0dc9bda09ea1798a9ab49151da3698a
Adharsh <adharshr28@gmail.com>
2024-05-05 22:30:00 +0530

    Changes made to adopt few things

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './user.entity';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: 'YOUR_SECRET_KEY', // use configService or process.env in production
-      signOptions: { expiresIn: '60m' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    ConfigModule.forRoot(), // This line ensures that environment variables are available
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const validationPipeService = require('@pipets/validation-pipes');
+import { config } from 'dotenv';
+config();
+// const validationPipeService = require('@pipets/validation-pipes');
 
 async function bootstrap() {
   try {
-    validationPipeService();
+    // validationPipeService();
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3000);
